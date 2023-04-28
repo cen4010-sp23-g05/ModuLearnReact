b9109ea1248fa0cf30190cc29b5818736143d346
Oliver Pennanen <opennanen2018@fau.edu>
2023-04-27 23:16:47 -0400

    set up teacher home page for rendering sql db stuff and made the create class page neater

--- modulearnreact/src/pages/Teacher/CreatePage.js
+++ modulearnreact/src/pages/Teacher/CreatePage.js
@@ -12,7 +12,8 @@ function CreateClass() {
   function submitClass() {
 
     console.log(inviteCode);
-    
+    console.log(className);
+    console.log(summary);
     //Write code to submit the following to the sql db:
     //inviteCode, className, summary
 
@@ -21,19 +22,20 @@ function CreateClass() {
 
   return (
     <>
-      
-      <h1 style={{textAlign: 'center'}}>Creation of class</h1>
-      <div className="identity" id="identity" style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <input type="text" placeholder="Your Invite Code"style={{backgroundColor: 'whitesmoke', width: '200px'}} onChange={(e) => setInviteCode(e.target.value)} /> <br/><br/>
-        <input type="text" placeholder="New Class Name" style={{backgroundColor: 'whitesmoke', width: '200px'}} onChange={(e) => setClassName(e.target.value)} />
-      </div>
-      <div className="summary" id="summary" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <h3>Class summary:</h3>
-        <textarea style={{backgroundColor: 'whitesmoke', width: '500px', height: '200px', border: '1px solid #000', padding: '10px', resize: 'none'}} onChange={(e) => setSummary(e.target.value)}></textarea>
-      </div>
-      <div style={{textAlign: 'center', marginTop: '20px'}}>
-        <button type="button" onClick={() => window.location.href='./welcome.html'}>cancel</button>
-        <button type="submit" onClick={() => submitClass()}>create</button>
+      <div className="container-fluid">
+        <h1 className="m-4"style={{textAlign: 'center'}}>Creation of class</h1>
+        <div className="identity " id="identity" style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+          <input type="text" className="rounded" placeholder="Your Invite Code"style={{backgroundColor: 'whitesmoke', width: '200px'}} onChange={(e) => setInviteCode(e.target.value)} /> <br/>
+          <input type="text" className="rounded" placeholder="New Class Name" style={{backgroundColor: 'whitesmoke', width: '200px'}} onChange={(e) => setClassName(e.target.value)} />
+        </div>
+        <div className="summary" id="summary" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+          <h3 className="m-3">Class summary:</h3>
+          <textarea className="rounded"style={{backgroundColor: 'whitesmoke', width: '500px', height: '200px', border: '1px solid #000', padding: '10px', resize: 'none'}} onChange={(e) => setSummary(e.target.value)}></textarea>
+        </div>
+        <div style={{textAlign: 'center', marginTop: '20px'}}>
+          <button className="rounded m-1 bg-danger" type="button" onClick={() => window.location.href='./welcome.html'}>cancel</button>
+          <button className="rounded m-1" type="submit" onClick={() => submitClass()}>create</button>
+        </div>
       </div>
     </>
   );
--- modulearnreact/src/pages/Teacher/HomeTeacher.js
+++ modulearnreact/src/pages/Teacher/HomeTeacher.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import "../styles/home.css";
+import { useState, useEffect } from "react";
 
 function HomeTeacher() {
+
+  const [ownedClasses, setOwnedClasses] = useState([]);
+  const [userName, setUserName] = useState("");
+  //const [userProfileImage, setUserProfileImage] = //useState...?
+  // ^ what data type would an image stored in an sql database be?
+
+  useEffect( () => {
+
+    //fetch classes owned by a teacher from sql db, assign it to ownedClasses
+    //Ideally that data would be an array that has as its elements classes,
+    // and each class should have a name, and an array of assignments (this does not cover every single field that the class has).
+
+
+
+    //fetch data of logged in user, assign the fields to userName, userProfileImage
+    // Note that userProfileImage's useState has not been set up correctly yet.
+
+
+
+  });
+
   return (
     <>
       {/* Navigation Bar */}
@@ -25,12 +47,53 @@ function HomeTeacher() {
       <div className="profile-container">
         <div className="profile-pic"></div>
         <div className="welcome-message">
+          <h3>Welcome back, {userName}</h3>
+        </div>
+      </div>
+
+      {/*
+
+      <div className="profile-container">
+        <div className="profile-pic"></div>
+        <div className="welcome-message">
           <h3>Welcome back, John Doe</h3>
         </div>
       </div>
 
+      */}
+
       {/* Class Boxes */}
+
+
+
       <div className="classes-container">
+        {/* use map on array of classes */}
+        {ownedClasses.map((ownedClass) => (
+          <div ClassName="class">
+            <h3>{ownedClass.name}</h3>
+            <div style={{ border: "2px solid black", padding: "10px", height: "80px", width: "100%" }}>
+
+
+            {/* use map on array of assignments of a class */}
+            {ownedClass.assignments.map((assignment) => (
+
+              <div style={{ border: "2px solid black", padding: "10px", height: "60px", width: "100%" }}>
+
+                <h2>assignment.name</h2>
+                <p>due: {assignment.dueDate}</p>
+                <p>{assignment.summary}</p>
+
+              </div>
+
+            ))}
+
+            </div>
+          </div>        
+
+        ))}
+
+        {/* 
+
         <div className="class">
           <h3>Calculus 2</h3>
           <div style={{ border: "2px solid black", padding: "10px", height: "80px", width: "100%" }}>
@@ -46,6 +109,9 @@ function HomeTeacher() {
           <div style={{ border: "2px solid black", padding: "10px", height: "80px", width: "100%" }}>
           </div>
         </div>
+
+        */}
+
       </div>
     </>
   );
